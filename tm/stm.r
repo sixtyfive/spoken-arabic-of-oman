f58871b91913ca52ab7f11b548bbb2b69c787078
J. R. Schmid <jrs+git@weitnahbei.de>
2017-04-09 19:56:10 +0200

    Running STM again with cleaned files.

---
+++ tm/stm.r
@@ -0,0 +1,25 @@
+#!/usr/bin/Rscript
+
+library(stm)
+library(readr)
+
+# Read the corpus from the text files it resides in and also store those files' names
+corpus.dir = '../corpus/cleaned'
+corpus.filenames = list.files(corpus.dir, pattern='OA.*', full.names=T) # Only the Omani Arabic texts!
+corpus.raw = lapply(corpus.filenames, read_file)
+
+# Feed the corpus into the topic modelling algorithm
+processed = textProcessor(corpus.raw)
+out = prepDocuments(processed$documents, processed$vocab, processed$meta, lower.thresh=1) # Don't throw away any terms as we don't have enough anyways
+models = stm(documents = out$documents, vocab = out$vocab, K = 5) # Max number of topics that can be determined
+models.selection_data = selectModel(out$documents, out$vocab, K=5)
+models.selected = models.selection_data$runout[[2]]
+
+# Plot list of topics and associated terms, then plot a comparison between the top two topics
+png('stm_output/model_info.png', width=1000, height=1000, unit='px')
+plotModels(models.selection_data)
+png('stm_output/summary.png', width=500, height=500, unit='px')
+plot(models.selected)
+png('stm_output/top_models_comparison.png', width=500, height=500, unit='px')
+plot(models.selected, type='perspectives', topics=c(2, 5))
+dev.off()
