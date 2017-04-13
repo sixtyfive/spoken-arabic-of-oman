4805cbc6ddcef59edd0a6b16dc36485fb6e68673
J. R. Schmid <jrs+git@weitnahbei.de>
2017-04-13 21:27:12 +0200

    Some trimming.

--- tm/stm.r
+++ tm/stm.r
@@ -8,28 +8,30 @@ library(stringr)
 corpus.dir = '../corpus/cleaned'
 output.dir = 'oa'
 corpus.raw = read.csv(paste0(corpus.dir, '/', 'oa.csv'))
-processed = textProcessor(corpus.raw$documents, corpus.raw) #, customstopwords=read_file('stoplist.txt'))
-threshold=2
+stop_words = str_split(read_file('stoplist.txt'), '\n')[[1]]
+processed = textProcessor(corpus.raw$documents, corpus.raw, customstopwords=stop_words)
+threshold=2 # don't throw away too many terms as we don't have enough text anyways
 num_topics = 6 # max number of topics that can be determined
+plot_size = 500 # width and height
 
-out = prepDocuments(processed$documents, processed$vocab, processed$meta, lower.thresh=threshold) # don't throw away any terms as we don't have enough anyways
+out = prepDocuments(processed$documents, processed$vocab, processed$meta, lower.thresh=threshold) 
 
 # not really relevant for such a small corpus, but good to keep around not to forget about
-png(paste0(output.dir, '/removal_scenarios.png'), width=1000, height=1000, unit='px')
+png(paste0(output.dir, '/removal_scenarios.png'), width=plot_size, height=plot_size, unit='px')
 plotRemoved(processed$documents, lower.thresh = seq(1, 10, by=1))
 dev.off()
 
 fit1 <- function() {
   fit = stm(documents=out$documents, vocab=out$vocab, K=num_topics, data=out$meta, init.type='Spectral')
-  png(paste0(output.dir, '/topic_summary.png'), width=1000, height=1000, unit='px')
+  png(paste0(output.dir, '/topic_summary.png'), width=plot_size, height=plot_size, unit='px')
   plot(fit)
   dev.off()
   lapply(1:num_topics, function(topic) {
-    png(paste0(output.dir, '/contrast', str_pad(topic, 2, pad='0'), '.png'), width=1000, height=1000, unit='px')
-    cloud(fit, topic=topic, scale=c(7,.5))
+    png(paste0(output.dir, '/contrast', str_pad(topic, 2, pad='0'), '.png'), width=plot_size, height=plot_size, unit='px')
+    cloud(fit, topic=topic, scale=c(15, .5))
     dev.off()
   })
-  # png('oatexts/top_models_comparison.png', width=1000, height=1000, unit='px')
+  # png('oatexts/top_models_comparison.png', width=plot_size, height=plot_size, unit='px')
   # plot(fit, type='perspectives', topics=c(1, 2))
 }
 
